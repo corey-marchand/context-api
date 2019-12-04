349406f77df5409cbcd87aa7ba16651a70d85b6b
Corey Marchand <corey.marchand@me.com>
2019-12-03 23:00:13 -0800

    practice assignment complete

--- practice/src/components/counter/counter.js
+++ practice/src/components/counter/counter.js
@@ -1,38 +1,46 @@
-import React, {useState} from 'react';
+import React from 'react';
+import { SettingsContext } from './counter-context';
 
-const Count = () => {
-
-  const [count, setCount] = useState(0);
-
-  const styles = {
-    container: {
-      alignItems: 'center',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-evenly',
-      width: '40%',
-    },
-    counter: {
-      display: 'inline-block',
-      fontSize: '1em',
-      border: '2px solid #444',
-      borderRadius: '50%',
-      lineHeight: '5em',
-      textAlign: 'center',
-      width: '5em',
-    },
-    button: {
-      height: '3em',
-    },
+class Count extends React.Component {
+  static contextType = SettingsContext;
+  
+  render(){
+    const styles = {
+      container: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        width: '40%',
+      },
+      counter: {
+        display: 'inline-block',
+        fontSize: '1em',
+        border: '2px solid #444',
+        borderRadius: '50%',
+        lineHeight: '5em',
+        textAlign: 'center',
+        width: '5em',
+      },
+      button: {
+        height: '3em',
+      },
+    }
+    
+    return (
+      <div style={styles.container}>
+        <button style={styles.button} onClick={this.context.decrement}> 
+          {" - "}
+        </button>
+        <span style={styles.counter}>{this.context.counter}</span>
+        <button style={styles.button} onClick={this.context.increment}> 
+          {" + "}
+        </button>
+      </div>
+    );
   };
+    
+}
 
-  return (
-    <div style={styles.container}>
-      <button style={styles.button} onClick={() => setCount(count-1)}> - </button>
-      <span style={styles.counter}>{count}</span>
-      <button style={styles.button} onClick={() => setCount(count+1)}> + </button>
-    </div>
-  );
-};
 
 export default Count;
